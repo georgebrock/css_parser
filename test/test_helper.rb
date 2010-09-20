71549504307b9706cb17fa13dc7e5285c45c28cf
George Brocklehurst <george.brocklehurst@gmail.com>
2010-09-20 17:40:18 +0100

    Test helper method for parsing CSS

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,11 @@ $: << File.join(File.dirname(__FILE__), *%w[ .. lib ])
 require 'css'
 require 'shoulda'
 require 'redgreen' if ENV['TM_PID'].nil?
+
+class Test::Unit::TestCase
+  def parse(css)
+    stylesheet = CSS.parse(css)
+    assert_not_nil stylesheet, "Failed to parse CSS: #{css}\n\nParser errors:\n  #{CSS.parser.terminal_failures.join("\n  ")}\n"
+    stylesheet
+  end
+end
